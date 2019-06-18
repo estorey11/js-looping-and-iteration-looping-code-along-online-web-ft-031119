9a88021469e35989d06e18d0a188aa381cb84bc3
Eric Storey <estorey@gmail.com>
2019-06-18 19:50:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function writeCards(names, event){
   let cards=[]
   for (let i = 0; i < names.length; i++) {
-    cards.push(`Thanks you, ${names[i]},  for the wonderful ${event} gift.`)
+    cards.push(`Thanks you, ${names[i]},  for the wonderful ${event} gift!`)
   }
   return cards
 }
