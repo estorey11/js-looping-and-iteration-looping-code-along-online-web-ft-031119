8e80a776ebc49e97aa1897f02648ecb922f70ecd
Eric Storey <estorey@gmail.com>
2019-06-18 19:42:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solutions in this file
 function writeCards(names, event){
-  
+  let cards=[]
+  for (let i = 0; i < names.length; i++) {
+    cards.`Wrapped ${gifts[i]} and added a bow!`)
+  }
 }
